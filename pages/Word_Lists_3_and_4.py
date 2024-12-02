8df1e1e18c02d7ec7e657bef197d8e55726c7079
aramarx <aramarx.us@gmail.com>
2024-12-02 15:35:39 -0600

    Update Word_Lists_3_and_4.py

--- pages/Word_Lists_3_and_4.py
+++ pages/Word_Lists_3_and_4.py
@@ -11,259 +11,81 @@ import time
 DIFF_JST_FROM_UTC = 9
 now = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
 
-ut_now = time.time()
+Words starting with 'A':
+abbreviate, abnormality, abode, abrasion, abundantly, academic, accessory, accordion, acidic, acne, acrobat, adhesive, admirable, adoption, adversary, affected, affliction, affordable, agenda, airport, alimony, allergic, alliance, alpaca, alphabetical, amateur, amplify, amusing, animate, anklebone, annex, antibacterial, antibiotic, anxiety, apparition, appease, applause, aptitude, aquamarine, arcade, arrangement, assortment, athletic, attractive, auditory, avalanche, avocado
 
-f = open('./list.txt','rb')
+Words starting with 'B':
+badminton, balky, barbarian, bareback, bargain, barrette, bashfulness, beacon, bedazzle, bedridden, beforehand, behavior, believable, beneficial, benevolent, biannual, bicultural, bicycle, billionaire, bimonthly, biodiversity, bionics, birthmark, blamable, blarney, blissful, blistering, bluebonnet, bolster, bonfire, boomerang, botulism, boulevard, bountiful, braggart, braille, brainstorm, brilliance, brisket, brooch, buffered, buffoonery, bulbous, bureau, burglarize
 
-list0 = pickle.load(f)
-list1 = pickle.load(f)
-list2 = pickle.load(f)
-list3 = pickle.load(f)
-list4 = pickle.load(f)
-list5 = pickle.load(f)
-list6 = pickle.load(f)
-list7 = pickle.load(f)
-list8 = pickle.load(f)
+Words starting with 'C':
+calculate, calendar, canopy, capitalism, cardiac, carnation, cartridge, cataract, cavernous, centimeter, ceremony, chaplain, charitable, choppiness, cinema, circulation, circumstance, clearance, clergy, clincher, closure, cohesion, coincidence, colander, columnist, combustion, commercial, communicable, commute, complaint, concentrate, concerto, confirmed, congratulate, connection, connive, consultation, convention, convoy, corrode, corruption, cramming, creative, critical, curiosity, currency, curtail
 
-consumption_list = pickle.load(f)
-hour_later_list = pickle.load(f)
+Words starting with 'D':
+damask, dauntless, debonair, debt, decagon, deceit, declining, deductible, deflate, deformity, dehydrate, delivery, democracy, deodorant, desperate, detestable, development, devotion, diagram, dictation, dietary, diligent, diorama, discipline, discreet, disembark, disinfect, dispensable, disregard, district, divergence, doleful, domain, dominance, dosage, downcast, draftsman, drone, dumpling, dwindle, dynasty
 
+Words starting with 'E':
+earliest, earphone, earsplitting, editorial, effective, egoism, elaborate, elapse, elasticity, electromagnet, eligible, emanate, embroidery, emergency, emotional, employee, encore, endear, endurance, energetic, engagement, enjoyably, enormity, entirety, environment, episode, equate, erase, escapism, estimate, ethical, everglade, evict, evidence, excel, exercising, exhale, existence, expenditure, experience, exploration, expound, extremity
 
-v1a = pickle.load(f)
-v1b = pickle.load(f)
-v1c = pickle.load(f)
-v2a = pickle.load(f)
-v2b = pickle.load(f)
-v2c = pickle.load(f)
-v3a = pickle.load(f)
-v3b = pickle.load(f)
-v3c = pickle.load(f)
-v4a = pickle.load(f)
-v4b = pickle.load(f)
-v4c = pickle.load(f)
-v5a = pickle.load(f)
-v5b = pickle.load(f)
-v5c = pickle.load(f)
-v6a = pickle.load(f)
-v6b = pickle.load(f)
-v6c = pickle.load(f)
-v7a = pickle.load(f)
-v7b = pickle.load(f)
-v7c = pickle.load(f)
-v8a = pickle.load(f)
-v8b = pickle.load(f)
-v8c = pickle.load(f)
+Words starting with 'F':
+fabulous, facedown, factorization, famish, fanciful, fatalism, fattened, federalist, feminine, ferocious, fiberglass, fictionalize, fidelity, fiercely, filbert, filtration, flagrant, flatterer, flounce, food chain, footbridge, foreclose, foreign, forerunner, forgery, forgetfulness, formative, fortitude, foyer, fraction, fragile, fragrance, frankfurter, fraternity, freebie, freedbee, freedom, frontier, functional, funeral, furlough, fuzziness
 
-list_start = pickle.load(f)
+Words starting with 'G':
+gangplank, gasoline, gaudy, gauze, gearless, gemstone, generality, generation, genetic, geographical, geometric, giddy, gingerly, glacier, gloominess, gluttony, goldenrod, good-humored, goodwill, gooseneck, gorgeous, govern, gradually, graffiti, granola, graphic, gravitation, greasier, greatness, greengrocer, griminess, grinning, grizzled, grouchy, guidance, guidebook, gumbo, gurgle
 
-    
+Words starting with 'H':
+habitable, haggard, hamstring, handicapped, handily, handlebar, happiness, happy-go-lucky, harmfully, hatchery, hauntingly, heatedly, heather, heatstroke, hedgehog, heighten, henceforth, hepatitis, herbicide, hexagon, hibachi, hideous, hindrance, hoist, hominid, homophone, honeycomb, hoopskirt, horoscope, hotheaded, hovercraft, humidity, hummingbird, husbandry, hydrology, hyena, hygienic, hyphen, hypnosis, hysterics
 
-#登録時間
-start = list_start[0]
-#消費率
-consumption_rate = consumption_list[0]
-#x時間後
-hour_later = hour_later_list[0]
-#x時間後の日時
-later_ = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC + hour_later_list[0])
-#経過時間
-progress = ((ut_now - start)/3600 )
-#消費量
-consumption = consumption_rate * (progress+hour_later)
-#20時間
-de = ut_now -start
+Words starting with 'I':
+icicle, idealism, identical, ideology, ignoring, illegal, imaginable, imitative, immense, immodest, immovable, impassable, impeach, impossible, improper, improvise, incidence, incision, inconvenience, indecision, independent, indicator, inedible, infatuate, inferior, inherent, injustice, innovative, instructor, insulation, insurance, interesting, intermittent, internist, intrusive, inventory, invigorate, invitation, irrational, irrigation, issue
 
-entY = list0[0]
-entm = list0[1]
-entd = list0[2]
-entH = list0[3]
-entM = list0[4]
+Words starting with 'J':
+jaguar, jamboree, jawbreaker, jellyfish, jetty, jitterbug, jobholder, joggled, joist, jubilation, juniper, justify
 
-print(start)
-#print(ut_now)
-#print (de)
+Words starting with 'K':
+kelp, kernel, kidney, kindhearted, kinship, knighthood, knitting, knockabout
 
-#######
-if '　←開' in v1a:
-    lv1a = (list1[0] - consumption)
-else:
-    lv1a = list1[0]
-lv1a = round(lv1a,2)   
-if '　←開' in v1b:
-    lv1b = (list1[1] - consumption)
-else:
-    lv1b = list1[1]
-lv1b = round(lv1b,2) 
-if '　←開' in v1c:
-    lv1c = (list1[2] - consumption)
-else:
-    lv1c = list1[2]
-lv1c = round(lv1c,2)
+Words starting with 'L':
+laboratory, lacerate, lamentation, laminate, landline, languid, larceny, lattice, lawlessly, layette, league, leastwise, leathery, lectern, leeway, legality, legislature, leisure, lemon, levelheaded, licorice, lifeline, light-year, limerick, lineage, liquefy, listener, lobbyist, locality, loneliness, loose, lottery, loudmouth, lumberyard, luminescent, luxurious, lynx
 
-if '　←開' in v2a:
-    lv2a = (list2[0] - consumption)
-else:
-    lv2a = list2[0]
-lv2a = round(lv2a,2)   
-if '　←開' in v2b:
-    lv2b = (list2[1] - consumption)
-else:
-    lv2b = list2[1]
-lv2b = round(lv2b,2) 
-if '　←開' in v2c:
-    lv2c = (list2[2] - consumption)
-else:
-    lv2c = list2[2]
-lv2c = round(lv2c,2)
+Words starting with 'M':
+magnetic, magnolia, mainstream, maize, malefactor, malformation, malicious, manageable, marathon, mascara, masterful, materialize, maturity, maximum, meaningful, medication, meditative, melodrama, membrane, memorial, mercenary, merchant, metallic, meteorologist, migratory, miniature, minivan, minority, misconception, misguidance, misspend, mistletoe, mistrust, monitor, monotone, mosquito, motley, multitude, murmur, mutate
 
-if '　←開' in v3a:
-    lv3a = (list3[0] - consumption)
-else:
-    lv3a = list3[0]
-lv3a = round(lv3a,2)   
-if '　←開' in v3b:
-    lv3b = (list3[1] - consumption)
-else:
-    lv3b = list3[1]
-lv3b = round(lv3b,2) 
-if '　←開' in v3c:
-    lv3c = (list3[2] - consumption)
-else:
-    lv3c = list3[2]
-lv3c = round(lv3c,2)  
+Words starting with 'N':
+nape, narcotic, narrator, nationalism, natural resource, navigable, navigator, necessitate, needful, neglectful, negotiate, neighborhood, nervy, nethermost, nettle, neutralize, newcomer, newspaperman, nifty, nightly, ninepin, nitpick, noiseless, nonchalant, nonprofit, nonsense, nonverbal, nonviolence, normalize, northeasterly, nostalgic, notoriety, nougat, novitiate, nozzle, nuisance, numeral, nurturant, nuthatch, nutlet, nutriment
 
-if '　←開' in v4a:
-    lv4a = (list4[0] - consumption)
-else:
-    lv4a = list4[0]
-lv4a = round(lv4a,2)   
-if '　←開' in v4b:
-    lv4b = (list4[1] - consumption)
-else:
-    lv4b = list4[1]
-lv4b = round(lv4b,2) 
-if '　←開' in v4c:
-    lv4c = (list4[2] - consumption)
-else:
-    lv4c = list4[2]
-lv4c = round(lv4c,2)
+Words starting with 'O':
+obese, obeying, obituary, oblivious, obscure, observant, obviously, occupation, odometer, officiate, olive, ominous, onslaught, opacity, openhearted, operating, opposable, optimal, optometry, orate, orbiter, orderliness, ordinary, oregano, organic, original, ornery, outburst, outlying, outwardly, outweigh, overestimate, override, oversupply, oxygen
 
-if '　←開' in v5a:
-    lv5a = (list5[0] - consumption)
-else:
-    lv5a = list5[0]
-lv5a = round(lv5a,2)   
-if '　←開' in v1b:
-    lv5b = (list5[1] - consumption)
-else:
-    lv5b = list5[1]
-lv5b = round(lv5b,2) 
-if '　←開' in v5c:
-    lv5c = (list5[2] - consumption)
-else:
-    lv5c = list5[2]
-lv5c = round(lv5c,2)
+Words starting with 'P':
+packaging, palpitate, panhandle, paradise, paradox, parakeet, paralysis, pathogen, patriotic, pedestal, pedicure, penalize, penetrate, penitence, pepperoni, percentage, perfection, perilous, perplexity, pesticide, petroleum, pictorial, pineapple, pinkie, pinky, plaintiff, plasticity, poisonous, policyholder, polyester, portable, portfolio, possession, practical, precinct, predestine, predicament, proactive, problematic, proceed, profession, prosperous, puzzling
 
-if '　←開' in v6a:
-    lv6a = (list6[0] - consumption)
-else:
-    lv6a = list6[0]
-lv6a = round(lv6a,2)   
-if '　←開' in v6b:
-    lv6b = (list6[1] - consumption)
-else:
-    lv6b = list6[1]
-lv6b = round(lv6b,2) 
-if '　←開' in v6c:
-    lv6c = (list6[2] - consumption)
-else:
-    lv6c = list6[2]
-lv6c = round(lv6c,2)
+Words starting with 'Q':
+quaintness, qualm, quarantine, quarterback, queasier, quick bread, quince, quitting, quizzes
 
-if '　←開' in v7a:
-    lv7a = (list7[0] - consumption)
-else:
-    lv7a = list7[0]
-lv7a = round(lv7a,2)   
-if '　←開' in v7b:
-    lv7b = (list7[1] - consumption)
-else:
-    lv7b = list7[1]
-lv7b = round(lv7b,2) 
-if '　←開' in v7c:
-    lv7c = (list7[2] - consumption)
-else:
-    lv7c = list7[2]
-lv7c = round(lv7c,2)
+Words starting with 'R':
+racketeer, radiantly, radical, railroad, ramshackle, raspy, rationale, realistic, reasoning, reassure, rebroadcast, rebuttal, receive, recession, reconcile, reconstruct, rectangular, reference, refrigerate, regardless, regiment, relentless, relevant, reluctantly, remnant, replacement, replica, reptilian, respectable, restaurant, retort, retriever, revenue, review, ricotta, ridiculous, roadrunner, rodent, rollicking, roughneck, rowdiness, rubella, russet
 
-if '　←開' in v8a:
-    lv8a = (list8[0] - consumption)
-else:
-    lv8a = list8[0]
-lv8a = round(lv8a,2)   
-if '　←開' in v8b:
-    lv8b = (list8[1] - consumption)
-else:
-    lv8b = list8[1]
-lv8b = round(lv8b,2) 
-if '　←開' in v8c:
-    lv8c = (list8[2] - consumption)
-else:
-    lv8c = list8[2]
-lv8c = round(lv8c,2)
-######
+Words starting with 'S':
+sabotage, salsa, sarcasm, satisfactory, scandal, scarcely, schedule, scorekeeper, scourge, seasonable, seclusion, sectional, sedative, seizure, semiarid, sensational, seriously, seventh, shrewd, siesta, simplicity, singular, situation, skittish, sociable, solidify, solstice, specific, spectacle, spectrum, splendid, squirm, statement, stationary, stereotype, strategy, stubborn, subjective, substantial, summary, supplement, survive, syllabicate, symbolism, synthetic
 
-#表示
+Words starting with 'T':
+taffeta, talkative, tastefully, taxation, technician, telescopic, temperament, tension, terrier, terrific, textual, theatrical, thermometer, thesis, threaten, thwart, tightwad, timberline, tincture, tinsel, toilsome, tollgate, tomorrow, topical, tousle, toxemia, tragedy, translate, treasurer, tremendous, triangular, trophy, trustworthy, tunnel, turbojet, twentieth, typewriter, typify
 
-if de >= 72000 :
-    st.subheader(f"本日未登録")
-    
-else:
-    st.subheader(f"**:red[{list0[0]}/{list0[1]}/{list0[2]}　{list0[3]}:{list0[4]}登録]**")
-    
-    col1, = st.columns(1)
-    with col1:
+Words starting with 'U':
+ultima, unaffected, unaligned, unbearable, unblemished, unclassified, underpass, unenclosed, uneventful, uniformity, university, unlined, unplug, unravel, unutterable, uproarious, usage, uttermost
 
-        st.subheader('---1号---')
-        st.subheader(f"A : {list1[0]}{v1a[0]}")
-        st.subheader(f"B : {list1[1]}{v1b[0]}")
-        st.subheader(f"C : {list1[2]}{v1c[0]}")
+Words starting with 'V':
+vaccinate, validity, vandalism, vanquish, vaporize, vegetative, velocity, vendetta, veneer, venture, version, veterinarian, victimize, vigilant, vindicate, visitation, vitality, vivid, vocation, volcanic, volume
 
-        st.subheader('---2号---')
-        st.subheader(f"A : {list2[0]}{v2a[0]}")
-        st.subheader(f"B : {list2[1]}{v2b[0]}")
-        st.subheader(f"C : {list2[2]}{v2c[0]}")
+Words starting with 'W':
+waistband, wallaby, warehouse, warrant, wash-and-wear, waspish, wearable, web-footed, wharf, wheelchair, wherefore, white blood cell, whitening, wireless, wisecrack, wittingly, woozy, workmanship
 
-        st.subheader('---3号---')
-        st.subheader(f"A : {list3[0]}{v3a[0]}")
-        st.subheader(f"B : {list3[1]}{v3b[0]}")
-        st.subheader(f"C : {list3[2]}{v3c[0]}")
+Words starting with 'X':
+xylophone
 
-        st.subheader('---4号---')
-        st.subheader(f"A : {list4[0]}{v4a[0]}")
-        st.subheader(f"B : {list4[1]}{v4b[0]}")
-        st.subheader(f"C : {list4[2]}{v4c[0]}")
+Words starting with 'Y':
+yacht, yearling
 
-        st.subheader('---5号---')
-        st.subheader(f"A : {list5[0]}{v5a[0]}")
-        st.subheader(f"B : {list5[1]}{v5b[0]}")
-        st.subheader(f"C : {list5[2]}{v5c[0]}")
-
-        st.subheader('---6号---')
-        st.subheader(f"A : {list6[0]}{v6a[0]}")
-        st.subheader(f"B : {list6[1]}{v6b[0]}")
-        st.subheader(f"C : {list6[2]}{v6c[0]}")
-
-        st.subheader('---7号---')
-        st.subheader(f"A : {list7[0]}{v7a[0]}")
-        st.subheader(f"B : {list7[1]}{v7b[0]}")
-        st.subheader(f"C : {list7[2]}{v7c[0]}")
-        
-        st.subheader('---8号---')
-        st.subheader(f"A : {list8[0]}{v8a[0]}")
-        st.subheader(f"B : {list8[1]}{v8b[0]}")
-        st.subheader(f"C : {list8[2]}{v8c[0]}")
-        st.write(f"---------") 
-        st.write(f"※左上の>からforecastをタップで予測値がみれる") 
-        st.write(f"※登録日時は要確認です、前日の情報を見ているかも")       
+Words starting with 'Z':
+zealous, zestfully
         st.write(f"※登録時刻から20時間経過すると自動で消去されます")
